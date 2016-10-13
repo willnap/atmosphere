fdd285d9fb58493927104c9a65a8e7a264aa5ca7
Will Napier <w.p.napier@gmail.com>
2016-10-13 04:59:51 -0600

    Add support for simplified Chinese

---
+++ locale/zh.php
@@ -0,0 +1,43 @@
+<?php
+$locale = array(
+  "Location Not Found" => "未找到位置",
+  "Hmm, the location you’re looking for can’t be found." => "您正在寻找的位置不能被发现。",
+  "Are you sure you didn’t mistype it?" => "你确定拼写正确吗？",
+  "It's Not You, It's Us." => "这不是你，这是我们。",
+  "We’re experiencing an internal error." => "我们正在经历一个内部错误。",
+  "The site administrator has been contacted." => "站点管理员已联系。",
+  "Weather for " => "天气 ",
+  "Weather for Current Location" => "天气当前位置",
+  "Not Supported" => "不支持",
+  "Search Error" => "搜索错误",
+  "Locating..." => "定位",
+  "Powered by Dark Sky" => "由Dark Sky提供",
+  "Created by Will Napier" => "创建由Will Napier",
+  "Source on Github" => "来源Github上",
+  "Chance of Rain" => "几率的雨",
+  "Chance of Snow" => "几率的雪",
+  "Chance of Sleet" => "几率的霰",
+  "Chance of Precipitation" => "几率的沉淀",
+  "Sunrise" => "日出",
+  "Sunset" => "日落",
+  "Feels like " => "感觉像",
+  "Today" => "今天",
+  "Atmosphere for " => "天气",
+  "Error" => "错误",
+  "select to change city" => "选择至更改城市",
+  "Enter a Location" => "输入一个位置",
+  "Change Location" => "换地点",
+  "Current Location" => "当前位置",
+  "Settings" => "设置",
+  "Units" => "温度单位",
+  "Hour Format" => "小时格式",
+  "12 Hour (AM, PM)" => "12小时（上午，下午）",
+  "24 Hour" => "24小时",
+  "Language (beta)" => "语言（测试版）",
+  "Save" => "保存",
+);
+$localeRTL = false;
+$locale24 = false;
+$locale_php = "zh_CN";
+$locale_geo = "zh-CN";
+?>
