de02913c40663072128c651d173508c96e2bc68a
Will Napier <w.p.napier@gmail.com>
2016-10-13 04:58:08 -0600

    Add support for French

---
+++ locale/fr.php
@@ -0,0 +1,43 @@
+<?php
+$locale = array(
+  "Location Not Found" => "Emplacement Non Trouvé",
+  "Hmm, the location you’re looking for can’t be found." => "L&#39;emplacement que vous cherchez ne peut pas être trouvé.",
+  "Are you sure you didn’t mistype it?" => "Êtes-vous sûr de ne pas mal orthographié il?",
+  "It's Not You, It's Us." => "Il est pas toi, ça nous.",
+  "We’re experiencing an internal error." => "Nous vivons une erreur interne.",
+  "The site administrator has been contacted." => "L&#39;administrateur du site a été contacté.",
+  "Weather for " => "Météo pour ",
+  "Weather for Current Location" => "Météo pour Situation Actuelle",
+  "Not Supported" => "Non Supporté",
+  "Search Error" => "Erreur de Recherche",
+  "Locating..." => "Localisation...",
+  "Powered by Dark Sky" => "Propulsé par Dark Sky",
+  "Created by Will Napier" => "Créé par Will Napier",
+  "Source on Github" => "Source sur Github",
+  "Chance of Rain" => "Risque de Pluie",
+  "Chance of Snow" => "Risque de Neige",
+  "Chance of Sleet" => "Risque de Grésil",
+  "Chance of Precipitation" => "Risques de Précipitations",
+  "Sunrise" => "Aube",
+  "Sunset" => "Coucher",
+  "Feels like " => "Se Sent Comme ",
+  "Today" => "Aujourd&#39;hui",
+  "Atmosphere for " => "Météo pour ",
+  "Error" => "Erreur",
+  "select to change city" => "sélectionnez pour changer la ville",
+  "Enter a Location" => "Entrez un Emplacement",
+  "Change Location" => "Changer de Lieu",
+  "Current Location" => "Localisation Actuelle",
+  "Settings" => "Paramètres",
+  "Units" => "Unités",
+  "Hour Format" => "Heure Format",
+  "12 Hour (AM, PM)" => "12 Heure (AM, PM)",
+  "24 Hour" => "24 Heure",
+  "Language (beta)" => "Langue (beta)",
+  "Save" => "Sauvegarder",
+);
+$localeRTL = false;
+$locale24 = true;
+$locale_php = "fr_FR";
+$locale_geo = "fr";
+?>
