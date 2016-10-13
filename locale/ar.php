dfb41f195ef0214e243d68a5c41989eb2e3032aa
Will Napier <w.p.napier@gmail.com>
2016-10-13 05:00:32 -0600

    Add support for Arabic

---
+++ locale/ar.php
@@ -0,0 +1,43 @@
+<?php
+$locale = array(
+  "Location Not Found" => "الموقع لم يتم العثور على",
+  "Hmm, the location you’re looking for can’t be found." => "الموقع الذي كنت تبحث عنها لا يمكن العثور عليه.",
+  "Are you sure you didn’t mistype it?" => "هل أنت متأكد أنك هجائه؟",
+  "It's Not You, It's Us." => "انها ليست لك، انها بنا.",
+  "We’re experiencing an internal error." => "فنحن نشهد خطأ داخلي.",
+  "The site administrator has been contacted." => "وقد تم الاتصال بمدير الموقع.",
+  "Weather for " => " الطقس ل",
+  "Weather for Current Location" => "حالة الطقس في المكان الحالي",
+  "Not Supported" => "غير معتمد",
+  "Search Error" => "خطأ",
+  "Locating..." => "تحديد موقع",
+  "Powered by Dark Sky" => "مدعوم من السماء المظلمة",
+  "Created by Will Napier" => "التي أنشأتها ويل نابير",
+  "Source on Github" => "",
+  "Chance of Rain" => "احتمال الأمطار",
+  "Chance of Snow" => "فرصة تساقط ثلوج",
+  "Chance of Sleet" => "فرصة للمطر متجمد",
+  "Chance of Precipitation" => "فرصة لهطول الأمطار",
+  "Sunrise" => "الشروق",
+  "Sunset" => "مغيب",
+  "Feels like " => "يشعر وكأنه",
+  "Today" => "اليوم",
+  "Atmosphere for " => " الطقس ل",
+  "Error" => "خطأ",
+  "select to change city" => "حدد لتغيير المدينة",
+  "Enter a Location" => "أدخل الموقع",
+  "Change Location" => "تغيير الموقع",
+  "Current Location" => "الموقع الحالي",
+  "Settings" => "إعدادات",
+  "Units" => "وحدات",
+  "Hour Format" => "شكل ساعة",
+  "12 Hour (AM, PM)" => "ساعة 12",
+  "24 Hour" => "24 ساعة",
+  "Language (beta)" => "لغة (بيتا)",
+  "Save" => "حفظ",
+);
+$localeRTL = true;
+$locale24 = false;
+$locale_php = "ar_AE";
+$locale_geo = "ar";
+?>
